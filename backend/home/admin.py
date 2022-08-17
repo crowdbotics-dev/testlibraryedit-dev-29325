df0150ddd982f3b9817f1c6cd19da4f38bf222d2
Crowdbotics <engineering@crowdbotics.com>
2022-08-17 07:14:44 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Test123
+
+admin.site.register(Test123)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Test123(models.Model):
+    "Generated Model"
+    t123 = models.BigIntegerField()
